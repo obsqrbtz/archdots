15ae35e17a80a8f4d8942b94e4f19c408185c80c
Daniel Dada <doesdeos@gmail.com>
2024-05-26 22:23:39 +0300

    Update install_deps.sh

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -24,23 +24,20 @@ sudo pacman -S stow \
 	kitty \
 	ttf-mononoki-nerd --noconfirm &&
 
-git clone https://aur.archlinux.org/swww.git swww &&
-cd swww &&
+git clone https://aur.archlinux.org/swww.git ~/swww &&
+cd ~/swww &&
 makepkg -si --noconfirm &&
-cd ../ &&
-rm -rf swww &&
+rm -rf ~/swww &&
 
-git clone https://aur.archlinux.org/fluent-gtk-theme.git fluent-gtk-theme &&
-cd fluent-gtk-theme &&
+git clone https://aur.archlinux.org/fluent-gtk-theme.git ~/fluent-gtk-theme &&
+cd ~/fluent-gtk-theme &&
 makepkg -si --noconfirm &&
-cd ../ &&
-rm -rf fluent-gtk-theme &&
+rm -rf ~/fluent-gtk-theme &&
 
-git clone https://aur.archlinux.org/bibata-cursor-theme-bin.git bibata-cursor-theme &&
-cd bibata-cursor-theme &&
+git clone https://aur.archlinux.org/bibata-cursor-theme-bin.git ~/bibata-cursor-theme &&
+cd ~/bibata-cursor-theme &&
 makepkg -si --nocomfirm &&
-cd ../ &&
-rm -rf bibata-cursor-theme &&
+rm -rf ~/bibata-cursor-theme &&
 
 chsh -s $(which zsh) $(whoami) &&
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" &&
