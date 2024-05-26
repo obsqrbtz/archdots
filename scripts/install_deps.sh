76435b785eff7dd9207d7dced81a7fca5d9c8c8c
Daniel Dada <doesdeos@gmail.com>
2024-05-26 22:37:40 +0300

    testing workflow

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -27,16 +27,19 @@ sudo pacman -S stow \
 git clone https://aur.archlinux.org/swww.git ~/swww &&
 cd ~/swww &&
 makepkg -si --noconfirm &&
+cd ../ &&
 rm -rf ~/swww &&
 
 git clone https://aur.archlinux.org/fluent-gtk-theme.git ~/fluent-gtk-theme &&
 cd ~/fluent-gtk-theme &&
 makepkg -si --noconfirm &&
+cd ../ &&
 rm -rf ~/fluent-gtk-theme &&
 
 git clone https://aur.archlinux.org/bibata-cursor-theme-bin.git ~/bibata-cursor-theme &&
 cd ~/bibata-cursor-theme &&
 makepkg -si --nocomfirm &&
+cd ../ &&
 rm -rf ~/bibata-cursor-theme &&
 
 chsh -s $(which zsh) $(whoami) &&
